b42f29ad2364a746f6fc270ed0f1d15fcf65b322
kvapehe <33888633+kvapehe@users.noreply.github.com>
2021-11-12 10:15:40 +0100

    Leveringsklar nedtelling 9 - 0 med musikk

--- main.ts
+++ main.ts
@@ -1,1 +1,30 @@
-
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(t01)
+    while (t01 > 0) {
+        music.playTone(131, music.beat(BeatFraction.Sixteenth))
+        basic.showNumber(t01)
+        basic.pause(500)
+        t01 += -1
+    }
+    for (let index = 0; index < 5; index++) {
+        basic.showIcon(IconNames.Heart)
+        basic.pause(500)
+        // Krever microbit V2
+        soundExpression.giggle.playUntilDone()
+        basic.showIcon(IconNames.SmallHeart)
+    }
+    basic.showIcon(IconNames.Heart)
+    music.playMelody("E B C5 A B G A F ", 359)
+    music.playMelody("E B C5 A B G A F ", 359)
+    music.playMelody("E - - - - - - - ", 81)
+})
+input.onButtonPressed(Button.B, function () {
+    t01 = 9
+    basic.showNumber(t01)
+})
+let t01 = 0
+t01 = 9
+basic.showNumber(t01)
+basic.forever(function () {
+	
+})
